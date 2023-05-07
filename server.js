99009c4af0dc6c9978be7e650f07122f470e9e03
Joe Lauletta <laulettaj@gmail.com>
2023-05-06 22:07:48 -0400

    add get route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "Hello World!" });  
+});
+
 app.listen(port, () => {
     console.clear();
     console.log(`Server running on port ${port}`);
